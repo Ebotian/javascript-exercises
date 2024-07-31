5d2447382dde227311d5e2364b38e42c277865b2
Ebotian <yiboxiaotian@nuaa.edu.cn>
2024-07-31 01:55:23 +0000

    567complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function (intStart, intEnd) {
+	if (
+		intStart < 0 ||
+		intEnd < 0 ||
+		typeof intStart !== "number" ||
+		typeof intEnd !== "number" ||
+		intStart % 1 !== 0 ||
+		intEnd % 1 !== 0
+	) {
+		return "ERROR";
+	}
+	if (intStart > intEnd) {
+		let temp = intStart;
+		intStart = intEnd;
+		intEnd = temp;
+	}
+	let sum = 0;
+	for (let i = intStart; i <= intEnd; i++) {
+		sum += i;
+	}
+	return sum;
 };
 
 // Do not edit below this line
